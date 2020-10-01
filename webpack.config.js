666bdcdf204b81ecf6cbb0e240eadcf612dd2e67
coemans <tom.coemans@gmail.com>
2020-10-01 11:50:55 +0200

    hide back to root button when already on root (#2)

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin
 module.exports = {
     mode: 'production',
     entry: {
-        'index': './src/index.js'
+        'index': './src/js/index.js'
     },
     output: {
         filename: '[name].bundle.[chunkhash].js',
@@ -37,133 +37,133 @@ module.exports = {
             template: 'src/html/400.html',
             chunks: ['index'],
             inlineSource: '.(js|css)$',
-            favicon: 'src/images/favicon.ico'
+            favicon: 'src/img/favicon.ico'
         }),
         new HtmlWebpackPlugin({
             filename: '401.html',
             template: 'src/html/401.html',
             chunks: ['index'],
             inlineSource: '.(js|css)$',
-            favicon: 'src/images/favicon.ico'
+            favicon: 'src/img/favicon.ico'
         }),
         new HtmlWebpackPlugin({
             filename: '403.html',
             template: 'src/html/403.html',
             chunks: ['index'],
             inlineSource: '.(js|css)$',
-            favicon: 'src/images/favicon.ico'
+            favicon: 'src/img/favicon.ico'
         }),
         new HtmlWebpackPlugin({
             filename: '404.html',
             template: 'src/html/404.html',
             chunks: ['index'],
             inlineSource: '.(js|css)$',
-            favicon: 'src/images/favicon.ico'
+            favicon: 'src/img/favicon.ico'
         }),
         new HtmlWebpackPlugin({
             filename: '405.html',
             template: 'src/html/405.html',
             chunks: ['index'],
             inlineSource: '.(js|css)$',
-            favicon: 'src/images/favicon.ico'
+            favicon: 'src/img/favicon.ico'
         }),
         new HtmlWebpackPlugin({
             filename: '408.html',
             template: 'src/html/408.html',
             chunks: ['index'],
             inlineSource: '.(js|css)$',
-            favicon: 'src/images/favicon.ico'
+            favicon: 'src/img/favicon.ico'
         }),
         new HtmlWebpackPlugin({
             filename: '410.html',
             template: 'src/html/410.html',
             chunks: ['index'],
             inlineSource: '.(js|css)$',
-            favicon: 'src/images/favicon.ico'
+            favicon: 'src/img/favicon.ico'
         }),
         new HtmlWebpackPlugin({
             filename: '411.html',
             template: 'src/html/411.html',
             chunks: ['index'],
             inlineSource: '.(js|css)$',
-            favicon: 'src/images/favicon.ico'
+            favicon: 'src/img/favicon.ico'
         }),
         new HtmlWebpackPlugin({
             filename: '412.html',
             template: 'src/html/412.html',
             chunks: ['index'],
             inlineSource: '.(js|css)$',
-            favicon: 'src/images/favicon.ico'
+            favicon: 'src/img/favicon.ico'
         }),
         new HtmlWebpackPlugin({
             filename: '413.html',
             template: 'src/html/413.html',
             chunks: ['index'],
             inlineSource: '.(js|css)$',
-            favicon: 'src/images/favicon.ico'
+            favicon: 'src/img/favicon.ico'
         }),
         new HtmlWebpackPlugin({
             filename: '414.html',
             template: 'src/html/414.html',
             chunks: ['index'],
             inlineSource: '.(js|css)$',
-            favicon: 'src/images/favicon.ico'
+            favicon: 'src/img/favicon.ico'
         }),
         new HtmlWebpackPlugin({
             filename: '415.html',
             template: 'src/html/415.html',
             chunks: ['index'],
             inlineSource: '.(js|css)$',
-            favicon: 'src/images/favicon.ico'
+            favicon: 'src/img/favicon.ico'
         }),
         new HtmlWebpackPlugin({
             filename: '500.html',
             template: 'src/html/500.html',
             chunks: ['index'],
             inlineSource: '.(js|css)$',
-            favicon: 'src/images/favicon.ico'
+            favicon: 'src/img/favicon.ico'
         }),
         new HtmlWebpackPlugin({
             filename: '501.html',
             template: 'src/html/501.html',
             chunks: ['index'],
             inlineSource: '.(js|css)$',
-            favicon: 'src/images/favicon.ico'
+            favicon: 'src/img/favicon.ico'
         }),
         new HtmlWebpackPlugin({
             filename: '502.html',
             template: 'src/html/502.html',
             chunks: ['index'],
             inlineSource: '.(js|css)$',
-            favicon: 'src/images/favicon.ico'
+            favicon: 'src/img/favicon.ico'
         }),
         new HtmlWebpackPlugin({
             filename: '503.html',
             template: 'src/html/503.html',
             chunks: ['index'],
             inlineSource: '.(js|css)$',
-            favicon: 'src/images/favicon.ico'
+            favicon: 'src/img/favicon.ico'
         }),
         new HtmlWebpackPlugin({
             filename: '504.html',
             template: 'src/html/504.html',
             chunks: ['index'],
             inlineSource: '.(js|css)$',
-            favicon: 'src/images/favicon.ico'
+            favicon: 'src/img/favicon.ico'
         }),
         new HtmlWebpackPlugin({
             filename: '505.html',
             template: 'src/html/505.html',
             chunks: ['index'],
             inlineSource: '.(js|css)$',
-            favicon: 'src/images/favicon.ico'
+            favicon: 'src/img/favicon.ico'
         }),
         new HtmlWebpackPlugin({
             filename: '506.html',
             template: 'src/html/506.html',
             chunks: ['index'],
             inlineSource: '.(js|css)$',
-            favicon: 'src/images/favicon.ico'
+            favicon: 'src/img/favicon.ico'
         }),
         new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin)
     ]
