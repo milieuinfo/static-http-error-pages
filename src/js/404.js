1d65b784c16671d3e9da5b480b54827e99bcf5a2
Tom Coemans <tom.coemans@gmail.com>
2021-01-05 12:54:02 +0100

    added custom text for seveso

---
+++ src/js/404.js
@@ -0,0 +1,27 @@
+(() => {
+    const customize = ({title, text}) => {
+        const error = document.getElementById('error');
+        error.setAttribute('data-vl-title', title);
+        error.insertAdjacentHTML('afterbegin', `
+            <p slot="text">${text}</p>
+            <div slot="actions"></div>
+        `);
+    }
+
+    const location = document.location;
+    const hostname = location.hostname;
+    switch (hostname) {
+        case 'ontwikkel.milieuinfo.be':
+        case 'oefen.milieuinfo.be':
+        case 'milieuinfo.be':
+            if (location.pathname.includes('/seveso/')) {
+                customize({
+                    title: 'Niet meer beschikbaar',
+                    text: 'De internettoepassing voor de bepaling van de Seveso-status wordt niet langer ter beschikking gesteld. Informatie omtrent de Seveso-statusbepaling kan gevonden worden op <a href="https://omgeving.vlaanderen.be/bepalen-van-de-seveso-status" target="_blank">https://omgeving.vlaanderen.be/bepalen-van-de-seveso-status</a>.<br/><br/>Voor bijkomende vragen omtrent de Seveso-statusbepaling gelieve contact op te nemen met het Team Externe Veiligheid via <a href="mailto:seveso@vlaanderen.be">seveso@vlaanderen.be</a>.'
+                });
+            }
+            break;
+        default:
+            break;
+    }
+})();
