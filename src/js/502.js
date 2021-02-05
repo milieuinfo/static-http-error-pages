c99a6404ea0d35e201dfdeb87558aa7d6ead9eb8
coemans <tom.coemans@vlaanderen.be>
2021-02-05 08:43:14 +0100

    added hosts (#9)

--- src/js/502.js
+++ src/js/502.js
@@ -14,7 +14,11 @@
         case 'omgevingsloket-oefen.omgeving.vlaanderen.be':
         case 'omgevingsloket2-oefen.omgeving.vlaanderen.be':
         case 'omgevingsloket-ontwikkel.omgeving.vlaanderen.be':
-        case 'localhost':
+        case 'omgevingsloketpubliek-ontwikkel.omgeving.vlaanderen.be':
+        case 'omgevingsloketpubliek2-ontwikkel.omgeving.vlaanderen.be':
+        case 'omgevingsloketpubliek-oefen.omgeving.vlaanderen.be':
+        case 'omgevingsloketpubliek2-oefen.omgeving.vlaanderen.be':
+        case 'omgevingsloketpubliek.omgeving.vlaanderen.be':
             customize({
                 title: 'Onderhoudswerkzaamheden',
                 text: 'Sorry, het omgevings-/publiek loket is niet beschikbaar wegens onderhoudswerkzaamheden. Voor meer info, bezoek <a href="https://www.omgevingsloketvlaanderen.be" target="_blank">https://www.omgevingsloketvlaanderen.be</a> en lees blokje "status loket". Klik op die pagina rechtsboven op "hulp nodig" voor contactgegevens.<br/><br/>We doen er alles aan om het omgevingsloket en het publiek loket zo snel mogelijk terug ter beschikking te hebben.' 
