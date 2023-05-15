96d27a0bc1a6a9bb0b74d88e2a4f8713a7b887e4
Lodewijk De Schuyter <lds@milieuinfo.be>
2023-05-15 12:13:47 +0200

    HLPD-64334

--- src/js/502.js
+++ src/js/502.js
@@ -14,14 +14,20 @@
         case 'omgevingsloket-oefen.omgeving.vlaanderen.be':
         case 'omgevingsloket2-oefen.omgeving.vlaanderen.be':
         case 'omgevingsloket-ontwikkel.omgeving.vlaanderen.be':
+        case 'omgevingsloket2-ontwikkel.omgeving.vlaanderen.be':
         case 'omgevingsloketpubliek-ontwikkel.omgeving.vlaanderen.be':
         case 'omgevingsloketpubliek2-ontwikkel.omgeving.vlaanderen.be':
         case 'omgevingsloketpubliek-oefen.omgeving.vlaanderen.be':
         case 'omgevingsloketpubliek2-oefen.omgeving.vlaanderen.be':
         case 'omgevingsloketpubliek.omgeving.vlaanderen.be':
+        case 'omgevingsloketinzage-ontwikkel.omgeving.vlaanderen.be':
+        case 'omgevingsloketinzage2-ontwikkel.omgeving.vlaanderen.be':
+        case 'omgevingsloketinzage-oefen.omgeving.vlaanderen.be':
+        case 'omgevingsloketinzage2-oefen.omgeving.vlaanderen.be':
+        case 'omgevingsloketinzage.omgeving.vlaanderen.be':
             customize({
                 title: 'Onderhoudswerkzaamheden',
-                text: 'Sorry, het omgevings-/publiek loket is niet beschikbaar wegens onderhoudswerkzaamheden. Voor meer info, bezoek <a href="https://www.omgevingsloketvlaanderen.be" target="_blank">https://www.omgevingsloketvlaanderen.be</a> en lees blokje "status loket". Klik op die pagina rechtsboven op "hulp nodig" voor contactgegevens.<br/><br/>We doen er alles aan om het omgevingsloket en het publiek loket zo snel mogelijk terug ter beschikking te hebben.' 
+                text: 'Sorry, het Omgevings-/Inzageloket is niet beschikbaar wegens onderhoudswerkzaamheden. Voor meer info, bezoek <a href="https://www.omgevingsloketvlaanderen.be" target="_blank">https://www.omgevingsloketvlaanderen.be</a> en lees blokje "status loket". Klik op die pagina rechtsboven op "hulp nodig" voor contactgegevens.<br/><br/>We doen er alles aan om het Omgevingsloket en het Inzageloket zo snel mogelijk terug ter beschikking te hebben.'
             });
             break;
         default:
